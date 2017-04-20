bbee979d9cb929709bb2c1f6321c916672272274
manolo72 <manoel.olivier@gmail.com>
2017-04-20 23:00:29 +0200

    Remove old components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-//import { CubeComponent } from './cube/cube.component';
-//import { PanoramaEquirectangularComponent } from './panorama-equirectangular/panorama-equirectangular.component';
 
 import { ThreeModule } from './three/three.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    //CubeComponent,
-    //PanoramaEquirectangularComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    ThreeModule
+    ThreeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
