516c2f24bb7ef2724a650ed67e1282cf3a05bc3d
Moaaa <manoel.olivier@gmail.com>
2019-04-14 21:34:43 +0200

    Couleur en fonction du type spectral

--- src/app/services/objects/stars/stars.service.ts
+++ src/app/services/objects/stars/stars.service.ts
@@ -12,10 +12,10 @@ export class StarsService {
     groupOfStars: THREE.Object3D;
     starsPoints: THREE.Points;
     loaded = false;
-    lastUpdate: Date;
+    basicMaterials: any;
 
     constructor(private catalogService: CatalogService) {
-        this.lastUpdate = new Date();
+        this.initMaterials();
     }
 
     initialize() {
@@ -55,13 +55,13 @@ export class StarsService {
             if (maxZ < record.Z) { maxZ = record.Z; }
         }
         geometry.addAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
-        const material = new THREE.PointsMaterial( { size: 1, sizeAttenuation: false, alphaTest: 1, transparent: false } );
-        material.color.setHSL( 0.33, 0.59, 0.80 );
+        const material = new THREE.PointsMaterial( { size: 1, color: 0xFFECDF, sizeAttenuation: false, alphaTest: 1, transparent: false } );
+        // material.color.setHSL( 0.33, 0.59, 0.80 );
         this.starsPoints = new THREE.Points( geometry, material );
     }
 
     private getNearest(camera: THREE.Camera, target: THREE.Vector3): any[] {
-        let z = [];
+        let nears = [];
         for (let i = 0; i < this.nbStars; i++) {
             const record = this.catalogService.stars[i];
             const pos = new THREE.Vector3(record.X, record.Y, record.Z);
@@ -69,19 +69,19 @@ export class StarsService {
             const pos2 = new THREE.Vector3(record.X, record.Y, record.Z);
             const angle = target2.sub(camera.position).angleTo(pos2.sub(camera.position));
             if (pos.distanceTo(camera.position) < 20 && angle < Math.PI / 4) {
-                z.push(record);
+                nears.push(record);
             }
         }
-        return z;
+        return nears;
     }
 
     private createSpheres(nearest: any[]): void {
         this.stars = [];
         this.groupOfStars.children = [];
-        const geometrySphere = new THREE.SphereBufferGeometry( 0.05, 32, 32 );
-        const materialSphere = new THREE.MeshBasicMaterial( {color: 0xffff00} );
+        const geometrySphere = new THREE.SphereBufferGeometry( 0.05, 32, 16 );
         
         nearest.forEach((near) => {
+            const materialSphere = this.getMaterialFromSpectrum(near);
             const star = new THREE.Mesh(geometrySphere, materialSphere);
             star.translateX(near.X);
             star.translateY(near.Y);
@@ -90,5 +90,32 @@ export class StarsService {
             this.stars.push(star);
             this.groupOfStars.add(star);
         });
+    }
+
+    private getMaterialFromSpectrum(near: any): THREE.MeshBasicMaterial {
+        let spectrum = 'Z';
+        if (near.Spectrum && near.Spectrum.length > 0) {
+            const idx0 = near.Spectrum.charAt(0);
+            if (this.basicMaterials[idx0]) {
+                spectrum = idx0;
+            }
+        }
+        return this.basicMaterials[spectrum];
+    }
+
+    private initMaterials(): void {
+        this.basicMaterials = {
+            Z: new THREE.MeshBasicMaterial( {color: 0xFFFFFF} ),
+            O: new THREE.MeshBasicMaterial( {color: 0x93B6FF} ),
+            B: new THREE.MeshBasicMaterial( {color: 0xA7C3FF} ),
+            A: new THREE.MeshBasicMaterial( {color: 0xD5E0FF} ),
+            F: new THREE.MeshBasicMaterial( {color: 0xF9F5FF} ),
+            G: new THREE.MeshBasicMaterial( {color: 0xFFECDF} ),
+            K: new THREE.MeshBasicMaterial( {color: 0xFFD6AC} ),
+            M: new THREE.MeshBasicMaterial( {color: 0xFFAA58} ),
+            L: new THREE.MeshBasicMaterial( {color: 0xFF7300} ),
+            T: new THREE.MeshBasicMaterial( {color: 0xFF3500} ),
+            Y: new THREE.MeshBasicMaterial( {color: 0x999999} )
+        };
     }
 }
