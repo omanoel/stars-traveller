d7856ac16dd3a7781efa2c41ccce9c10d310b442
Moaaa <manoel.olivier@gmail.com>
2020-03-29 20:55:09 +0200

    Add catalogLocalPath for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  catalogUrl: 'https://raw.githubusercontent.com/astronexus/HYG-Database/master/hygdata_v3.csv'
+  catalogLocalPath: null,
+  catalogUrl:
+    'https://raw.githubusercontent.com/astronexus/HYG-Database/master/hygdata_v3.csv'
 };
